7d4556ee44ed3a0570403c53901e38e3f40f03b7
developeralamgirbd <developer.alamgirbd@gmail.com>
2024-06-22 21:38:08 +0600

    [Done] esim table and create form

--- src/pages/dashboard/ESIM.js
+++ src/pages/dashboard/ESIM.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import EsimList from "../../components/EsimList";
+import ESIMForm from "../../components/ESimFrom";
+import {Tabs} from "antd";
 
 const EsimCatalog = () => {
+
+    const [eSIMData, setESIMData] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        (async () => {
+            getEsim()
+        })()
+    }, []);
+
+
+    const getEsim = async ()=> {
+        setLoading(true)
+        const {data} = await axios.get('/esim');
+        setESIMData(data)
+        setLoading(false)
+    }
+
+
+    const items = [
+        {
+            key: '1',
+            label: 'List',
+            children: (
+
+                <EsimList eSIMData={eSIMData} loading={loading}/>
+
+            ) ,
+        },
+        {
+            key: '2',
+            label: 'Create New',
+            children: (
+
+                <ESIMForm/>
+
+            ),
+        }
+
+    ];
+
+
+    const handleOnchange = ()=> {
+        getEsim()
+    }
+
     return (
-        <div>
-            ESIM
-        </div>
+        <>
+            <Tabs defaultActiveKey="1" items={items} onChange={handleOnchange} />
+        </>
     );
 };
 
--- src/pages/dashboard/UserDashboard-page.js
+++ src/pages/dashboard/UserDashboard-page.js
@@ -5,56 +5,11 @@ import EsimList from "../../components/EsimList";
 import axios from "axios";
 
 const UserDashboardPage = () => {
-    const [eSIMData, setESIMData] = useState([])
-    const [loading, setLoading] = useState(false)
 
-    useEffect(() => {
-        (async () => {
-            getEsim()
-        })()
-    }, []);
-
-
-    const getEsim = async ()=> {
-        setLoading(true)
-        const {data} = await axios.get('/esim');
-        setESIMData(data)
-        setLoading(false)
-    }
-
-
-    const items = [
-        {
-            key: '1',
-            label: 'List',
-            children: (
-
-                    <EsimList eSIMData={eSIMData} loading={loading}/>
-
-            ) ,
-        },
-        {
-            key: '2',
-            label: 'Create New',
-            children: (
-
-                        <ESIMForm/>
-
-            ),
-        }
-
-    ];
-
-
-    const handleOnchange = ()=> {
-        getEsim()
-    }
 
     return (
 
-                <Tabs defaultActiveKey="1" items={items} onChange={handleOnchange} />
-
-
+            <div className='d-flex justify-content-center align-items-center vh-100 fs-1'>Welcome to Dashboard</div>
     );
 };
 
